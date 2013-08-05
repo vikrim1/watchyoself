bedd95dc2203f5611fb3e1738ed51a6298ae1fb6
Daniel Marcotte <dmarcotte@gmail.com>
2013-08-05 14:43:21 -0700

    Fix HTTP->HTTPS redirect in AWS
    In deployed environments, tell Spring Security to check the
    "X-Forwarded-Proto" header set by our load balancer rather than the url
    scheme.
    
    Also relocate all the spring security settings to be before our env
    specific settings so that they can be easily overriden in the future

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -69,10 +69,44 @@ grails.plugins.bvcommon.aws.bv.nexus.role='instance-role'
 grails.plugins.bvcommon.aws.bv.nexus.ec2.key.name='team-ec2-key'
 grails.plugins.bvcommon.aws.instance.type='m1.medium'
 
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.bazaarvoice.portal.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.bazaarvoice.portal.UserRole'
+grails.plugins.springsecurity.authority.className = 'com.bazaarvoice.portal.Role'
+grails.plugins.springsecurity.rejectIfNoRule = true
+grails.plugins.springsecurity.portMapper.httpPort = httpPort
+grails.plugins.springsecurity.portMapper.httpsPort = httpsPort
+// default to more secure rejectIfNoRule strategy
+grails.plugins.springsecurity.rejectIfNoRule = true
+// require https for all calls; also redirects http calls to https (http://grails-plugins.github.com/grails-spring-security-core/docs/manual/guide/17%20Channel%20Security.html)
+grails.plugins.springsecurity.secureChannel.definition = [
+        '/**':         'REQUIRES_SECURE_CHANNEL'
+]
+
+// PORTAL-GRAILS TODO you almost certainly want to change this approach to request mapping.  See you options here: http://grails-plugins.github.io/grails-spring-security-core/docs/manual/guide/5%20Configuring%20Request%20Mappings%20to%20Secure%20URLs.html
+grails.plugins.springsecurity.securityConfigType = "InterceptUrlMap"
+grails.plugins.springsecurity.interceptUrlMap = [
+        '/login/denied':     ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/**':            ['ROLE_ADMIN']
+]
+
+// Tell spring security to check the "X-Forwarded-Proto" set by our load balancer for https rather than
+// the url scheme
+grails.plugins.springsecurity.secureChannel.useHeaderCheckChannelSecurity = true
+// load balancer ports to use in deployed environments
+grails.plugins.springsecurity.portMapper.httpPort = 80
+grails.plugins.springsecurity.portMapper.httpsPort = 443
+
 environments {
     development {
         grails.logging.jul.usebridge = true
         grails.serverURL = "https://sample.portal.bazaarvoice.com:" + httpsPort
+
+        // disable the "X-Forwarded-Proto" in development since there's no load balancer
+        grails.plugins.springsecurity.secureChannel.useHeaderCheckChannelSecurity = false
+        // ensure spring security knows what ports we're serving from so it can do its redirects properly
+        grails.plugins.springsecurity.portMapper.httpPort = httpPort
+        grails.plugins.springsecurity.portMapper.httpsPort = httpsPort
     }
     test {
         // use "test" for BV "dev" environment, since "dev" is used in Grails for local development
@@ -114,24 +148,3 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
-
-// Added by the Spring Security Core plugin:
-grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.bazaarvoice.portal.User'
-grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.bazaarvoice.portal.UserRole'
-grails.plugins.springsecurity.authority.className = 'com.bazaarvoice.portal.Role'
-grails.plugins.springsecurity.rejectIfNoRule = true
-grails.plugins.springsecurity.portMapper.httpPort = httpPort
-grails.plugins.springsecurity.portMapper.httpsPort = httpsPort
-// default to more secure rejectIfNoRule strategy
-grails.plugins.springsecurity.rejectIfNoRule = true
-// require https for all calls; also redirects http calls to https (http://grails-plugins.github.com/grails-spring-security-core/docs/manual/guide/17%20Channel%20Security.html)
-grails.plugins.springsecurity.secureChannel.definition = [
-        '/**':         'REQUIRES_SECURE_CHANNEL'
-]
-
-// PORTAL-GRAILS TODO you almost certainly want to change this approach to request mapping.  See you options here: http://grails-plugins.github.io/grails-spring-security-core/docs/manual/guide/5%20Configuring%20Request%20Mappings%20to%20Secure%20URLs.html
-grails.plugins.springsecurity.securityConfigType = "InterceptUrlMap"
-grails.plugins.springsecurity.interceptUrlMap = [
-        '/login/denied':     ['IS_AUTHENTICATED_ANONYMOUSLY'],
-        '/**':            ['ROLE_ADMIN']
-]
