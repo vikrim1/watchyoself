dfa5261e68531977ed46dd67a365d20c47318bd3
Daniel Marcotte <dmarcotte@gmail.com>
2014-01-16 15:05:27 -0800

    Eliminate unreliable calls to setUp/@Before
    Since we can't guarantee that our setUp/@Before methods run before the
    ones provided by the Grails mixins (and we often NEED them to), stop
    using them, and enhance our scaffolding templates to contain a note
    discouraging their use

--- src/templates/testing/DomainClass.groovy
+++ src/templates/testing/DomainClass.groovy
@@ -3,8 +3,13 @@
 import grails.test.mixin.*
 import org.junit.*
 
+
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ *
+ * NOTE: do not use setUp/tearDown or @Before/@After
+ *     They are not guaranteed to run after Grails Test Mixin setUp/tearDown, and we often need them to.
+ *     See http://grails.1312388.n4.nabble.com/Test-mixins-and-Before-td4329905.html for details
  */
 @TestFor(@artifact.testclass@)
 class @artifact.name@ {
--- src/templates/testing/Filters.groovy
+++ src/templates/testing/Filters.groovy
@@ -2,6 +2,13 @@
 
 import grails.test.mixin.*
 
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ *
+ * NOTE: do not use setUp/tearDown or @Before/@After
+ *     They are not guaranteed to run after Grails Test Mixin setUp/tearDown, and we often need them to.
+ *     See http://grails.1312388.n4.nabble.com/Test-mixins-and-Before-td4329905.html for details
+ */
 @Mock(@artifact.testclass@)
 class @artifact.name@ {
 
